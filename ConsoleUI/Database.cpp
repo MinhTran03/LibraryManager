e5e22c42447df3add271b4ad9d8c87f8444310c9
Khoa Vu <khoavu882@gmail.com>
2019-07-12 11:49:08 +0700

    Minh lone

--- ConsoleUI/Database.cpp
+++ ConsoleUI/Database.cpp
@@ -3,10 +3,14 @@
 bool LoadDauSach(LIST_DAUSACH& listDS)
 {
 	return listDS.ReadFromFile("D:\\C++\\LibraryManager\\ConsoleUI\\DauSachData.txt");
+	return listDS.ReadFromFile("D:\\WorkSpace\\LibraryManager\\ConsoleUI\\DauSachData.txt");
+
 }
 bool LoadSach(LIST_SACH& listSach, char isbn[ISBN_MAXSIZE + 1])
 {
 	std::string path = "D:\\C++\\LibraryManager\\ConsoleUI\\DanhMucSach\\";
+	std::string path = "D:\\WorkSpace\\LibraryManager\\ConsoleUI\\DanhMucSach\\";
+
 	path += isbn;
 	path += ".txt";
 	return listSach.ReadFromFile(path);
@@ -14,5 +18,7 @@ bool LoadSach(LIST_SACH& listSach, char isbn[ISBN_MAXSIZE + 1])
 bool LoadDocGia(LIST_DOCGIA& listDocGia)
 {
 	std::string path = "D:\\C++\\LibraryManager\\ConsoleUI\\DocGiaData.txt";
+	std::string path = "D:\\WorkSpace\\LibraryManager\\ConsoleUI\\DocGiaData.txt";
+
 	return ReadFromFile(listDocGia, path);
 }
