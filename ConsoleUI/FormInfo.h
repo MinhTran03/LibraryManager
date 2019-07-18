347104f213b0bdea3c5ebf01f31f2a9f91616471
Khoa Vu <khoavu882@gmail.com>
2019-07-18 11:15:39 +0700

    cc

--- ConsoleUI/FormInfo.h
+++ ConsoleUI/FormInfo.h
@@ -13,4 +13,14 @@ void DocGiaInfo(RECTANGLE rect, DOCGIA docGia)
 	std::vector<std::string> labels = { "Ma doc gia:", "Ho:", "Ten:", "Gioi tinh:", "Trang thai the:" };
 	DrawRectangleBoder(rect);
 
+	for (size_t i = 0; i < docGiaInfo.size(); i++)
+	{
+		GoToXY(rect.location.x + 2, rect.location.y + 1);
+		cout << labels[i];
+		GoToXY(rect.location.x + 8, rect.location.y + 1);
+		cout << ":";
+		GoToXY(rect.location.x + 10, rect.location.y + 1);
+		cout << docGiaInfo[i];
+	}
+
 }
